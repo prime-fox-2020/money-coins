2bd9aa42f25f868ca9937b5962152ec37a39224a
bzxnaga <bzxnaga@gmail.com>
2020-03-30 19:18:48 +0700

    done

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function moneyCoins(money) {
   // your implementation code here
+  let coins = [10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 1];
+  let result = [];
+
+for(var i = 0 ; i < coins.length ; i ++){
+  while(money >= coins[i]){
+      result.push(coins[i]);
+      money = money - coins[i];
+  }
+}
+  return result;
 }
 
 //driver code
