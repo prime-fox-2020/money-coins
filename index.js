aa24e5fef0301cde85c733fc4f56c9d75b5cd451
akmaliavitri <akmaliavitri21@gmail.com>
2020-03-30 14:19:56 +0700

    money-coins

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 function moneyCoins(money) {
   // your implementation code here
+  //let pecahan = [1, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000]
+  let pecahan = [10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 1]
+  let result = []
+
+  while (money > 0){
+    for(let i=0; i<pecahan.length; i++){
+      if(money >= pecahan[i]){
+        result.push(pecahan[i])
+        money -= pecahan[i]
+        break
+      }
+    }
+  }
+  return result
 }
+  
 
 //driver code
 console.log(moneyCoins(543));
