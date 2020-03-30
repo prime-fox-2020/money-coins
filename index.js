eedb04c6d2e23068331819444750f4d6f26972c2
Muhammad Risky Nugraha <muhammadrisky83@gmail.com>
2020-03-30 11:41:31 +0700

    done

--- index.js
+++ index.js
@@ -1,5 +1,54 @@
 function moneyCoins(money) {
-  // your implementation code here
+  let res = []
+  for (let i = 0; i < Infinity; i++) {
+    if(money > 10000){
+      res.push(10000);
+      money %= 10000;
+    }
+    if(money > 5000){
+      res.push(5000);
+      money %= 5000;
+    }
+    if(money > 2000){
+      res.push(2000);
+      money %= 2000;
+    }
+    if(money > 1000){
+      res.push(1000);
+      money %= 1000;
+    }
+    if(money > 500){
+      res.push(500);
+      money %= 500;
+    }
+    if(money > 200){
+      res.push(200);
+      money %= 200;
+    }
+    if(money > 100){
+      res.push(100);
+      money %= 100;
+    }
+    if(money > 50){
+      res.push(50);
+      money %= 50;
+    }
+    if(money > 20){
+      res.push(20);
+      money %= 20;
+    }
+    if(money > 10){
+      res.push(10);
+      money %= 10;
+    }
+    if(money >= 1){
+      res.push(1);
+      money -= 1;
+    }
+    if(money === 0){
+      return res;
+    }
+  }
 }
 
 //driver code
