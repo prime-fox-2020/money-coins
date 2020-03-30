1ebe8c3f266af859c057124664a201b3a76bfe58
Vinita Dewi <vinitaadewi@gmail.com>
2020-03-30 16:44:22 +0700

    finished money-coin

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 function moneyCoins(money) {
+  var output = [];
   // your implementation code here
+  while (money > 0) {
+    if (money >= 10000) {
+      output.push(10000)
+      money -= 10000
+    } else if (money >= 5000) {
+      output.push(5000)
+      money -= 5000
+    } else if (money >= 2000) {
+      output.push(2000)
+      money -= 2000
+    } else if (money >= 1000) {
+      output.push(1000)
+      money -= 1000
+    } else if (money >= 500) {
+      output.push(500)
+      money -= 500
+    } else if (money >= 200) {
+      output.push(200)
+      money -= 200
+    } else if (money >= 100) {
+      output.push(100)
+      money -= 100
+    } else if (money >= 50) {
+      output.push(50)
+      money -= 50
+    } else if (money >= 20) {
+      output.push(20)
+      money -= 20
+    } else if (money >= 10) {
+      output.push(10)
+      money -= 10
+    } else if (money >= 1) {
+      output.push(1)
+      money -= 1
+    }
+  }
+  return output
 }
 
 //driver code
