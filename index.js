0da9e778a5074ca811a83b1b8cf16830859efd56
Xaber <getar.metal@gmail.com>
2020-03-30 17:05:13 +0700

    first commit, money-coins

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-function moneyCoins(money) {
+function convertToCoin(money) {
   // your implementation code here
+  var coin = [1, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000];
+  var result = [];
+
+  for (let a = coin.length - 1; a >= 0; a--) {
+    while (money >= coin[a]) {
+      result.push(coin[a]);
+      money -= coin[a];
+    }
+  }
+  return result;
 }
 
 //driver code
-console.log(moneyCoins(543));
+console.log(convertToCoin(543));
 //output : [ 500, 20, 20, 1, 1, 1 ]
 
-console.log(moneyCoins(7752));
+console.log(convertToCoin(7752));
 //output : [ 5000, 2000, 500, 200, 50, 1, 1 ]
 
-console.log(moneyCoins(37454));
+console.log(convertToCoin(37454));
 //output : [ 10000, 10000, 10000, 5000, 2000, 200, 200, 50, 1, 1, 1, 1 ]
 
 //abaikan code dibawah ini
-module.exports = moneyCoins
+module.exports = convertToCoin
