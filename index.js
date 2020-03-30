13584816e48757a5409dc557a54304c110776191
fadhlanfm <fafadhlanriz@gmail.com>
2020-03-30 20:04:42 +0700

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 function moneyCoins(money) {
   // your implementation code here
+  let moneyLeft = money
+  let pecahan = [10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 1]
+  let output = []
+
+  while(moneyLeft > 0){
+    for(let i = 0; i < pecahan.length; i++){
+      if(moneyLeft >= pecahan[i]){
+        output.push(pecahan[i])
+        moneyLeft -= pecahan[i]
+        break
+      }
+    }
+  }
+  return output
 }
 
 //driver code
