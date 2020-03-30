13d813bf5fd2cd61fe6b1936e2138db203f7e3b8
elvanrafif <terlanjurbuta@gmail.com>
2020-03-30 17:30:53 +0700

    add tugas money-coins

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function moneyCoins(money) {
-  // your implementation code here
+  var listMoney = [10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 1];
+  var output = [];
+  for (var i = 0; i < listMoney.length; i++) {
+    while (money >= listMoney[i]) {
+      output.push(listMoney[i]);
+      money -= listMoney[i];
+    }
+  }
+  return output;
 }
 
 //driver code
@@ -13,4 +21,4 @@ console.log(moneyCoins(37454));
 //output : [ 10000, 10000, 10000, 5000, 2000, 200, 200, 50, 1, 1, 1, 1 ]
 
 //abaikan code dibawah ini
-module.exports = moneyCoins
+module.exports = moneyCoins;
